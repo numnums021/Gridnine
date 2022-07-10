66cf9a5dc83fb0cd2b01fcfb1e3ffa1c4f18b48e
danya <numnums021@gmail.com>
2022-07-10 16:32:13 +0300

    rev

--- src/com/gridnine/testing/FlightFiltering.java
+++ src/com/gridnine/testing/FlightFiltering.java
@@ -3,32 +3,35 @@ package com.gridnine.testing;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FlightFiltering {
 
-    public static List<Flight> filterDepartureBeforeCurrentDate(List<Flight> flightList) {
+    public List<Flight> filterDepartureBeforeCurrentDate(List<Flight> flightList) {
         List<Flight> newFlightList = new ArrayList<>();
         for (int i = 0; i < flightList.size(); i++) {
             List<Segment> s = flightList.get(i).getSegments();
             for (int j = 0; j < s.size(); j++) {
                 if (s.get(j).getDepartureDate().isBefore(LocalDateTime.now())) {
-                    flightList.remove(i);
+                    s.remove(j);
                     break;
                 }
             }
             newFlightList.add(new Flight(s));
         }
+
         return newFlightList;
-        //flightList.stream().sorted(s -> s.getSegments().);
     }
 
-    public static List<Flight> filterArrivalDateBeforeDepartureDate(List<Flight> flightList) {
+    public List<Flight> filterArrivalDateBeforeDepartureDate(List<Flight> flightList) {
         List<Flight> newFlightList = new ArrayList<>();
         for (int i = 0; i < flightList.size(); i++) {
             List<Segment> s = flightList.get(i).getSegments();
             for (int j = 0; j < s.size(); j++) {
                 if (s.get(j).getDepartureDate().isAfter(s.get(j).getArrivalDate())) {
-                    flightList.remove(i);
+                    s.remove(j);
                     break;
                 }
             }
@@ -38,15 +41,17 @@ public class FlightFiltering {
         return newFlightList;
     }
 
-    public static List<Flight> filterNumberOfHoursOnEarth(List<Flight> flightList) {
+    public List<Flight> filterNumberOfHoursOnEarth(List<Flight> flightList) {
         for (int i = 0; i < flightList.size(); i++) {
             List<Segment> s = flightList.get(i).getSegments();
             for (int j = 0; j < s.size() - 1; j++) {
                 double beforeHours = s.get(j).getArrivalDate().getHour()
-                        + s.get(j).getArrivalDate().getMinute() * 60 + s.get(j).getArrivalDate().getSecond() * 360;
+                        + (s.get(j).getArrivalDate().getMinute() / 60.0)
+                        + (s.get(j).getArrivalDate().getSecond() / 3600.0);
                 double afterHours = s.get(j + 1).getDepartureDate().getHour()
-                        + s.get(j + 1).getArrivalDate().getMinute() * 60 + s.get(j + 1).getArrivalDate().getSecond() * 360;
-                if (afterHours - beforeHours < 2) {
+                        + s.get(j + 1).getDepartureDate().getMinute() / 60.0
+                        + (s.get(j + 1).getDepartureDate().getSecond() / 3600.0);
+                if (afterHours - beforeHours >= 2 && afterHours > 0) {
                     flightList.remove(i);
                     break;
                 }
@@ -54,5 +59,9 @@ public class FlightFiltering {
         }
 
         return flightList;
+    }
+
+    public void printFlight(List<Flight> flightList) {
+        flightList.stream().forEach(System.out::println);
     }
 }
